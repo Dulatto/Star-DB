85ec1736a75f499ad0a9c28a7d01a4ca9c9e5872
Dulatto <dulat.omarov@gmail.com>
2021-03-06 21:23:50 -0500

    initial state

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+  return body
+}
+
+getResource('https://swapi.dev/api/people/1/')
+.then((body) => {
+    console.log(body);
+})
+
+fetch('https://swapi.dev/api/people/1/')
+    .then((res) => {
+        return res.json();
+    })
+    .then((body) => {
+        console.log(body);
+    });
