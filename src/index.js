eb40eab53a6a0518e41e288619c00fbe0c06ff40
Dulatto <dulat.omarov@gmail.com>
2021-03-06 21:32:45 -0500

    initial state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok){
+       throw new Error(`Could not fetch ${url}` + 
+       `,received ${res.status}`)
+    }
+
     const body = await res.json();
   return body
 }
@@ -8,11 +14,6 @@ getResource('https://swapi.dev/api/people/1/')
 .then((body) => {
     console.log(body);
 })
-
-fetch('https://swapi.dev/api/people/1/')
-    .then((res) => {
-        return res.json();
-    })
-    .then((body) => {
-        console.log(body);
-    });
+.catch((err) =>{
+    console.log('Could not fetch', err);
+})
