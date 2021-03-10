dcc51ee19bd172bee0a5c03e357bedee9c822183
Dulatto <dulat.omarov@gmail.com>
2021-03-09 23:49:59 -0500

    error indicator

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import ErrorIndicator from '../error-indicator/error-indicator';
 import Spinner from '../spinner/spinner';
 
 import './random-planet.css';
@@ -21,27 +22,38 @@ export default class RandomPlanet extends Component {
    onPlanetLoaded = (planet) => {
    this.setState({ 
      planet,
-    loading:false });
+    loading:false,
+    error: false });
+  };
+
+  onError =(err) => {
+   this.setState({
+     error: true,
+     loading: false
+   });
   };
 
   updatePlanet() {
     const id = 12;
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
-  
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
-  render() {
-    
-    const {planet, loading} = this.state;
+  render() {    
+    const {planet, loading, error} = this.state;
 
+    const hasData = !(loading || error);
+
+  const errorMessage = error ? <ErrorIndicator /> : null;
   const spinner = loading ? <Spinner /> : null;
-  const content = !loading ? <PlanetView planet={planet}/> : null;
+  const content = hasData ? <PlanetView planet={planet}/> : null;
      
      
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
