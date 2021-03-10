92f21c70dc8b5d984cfd3141be5bc6e9a371807b
Dulatto <dulat.omarov@gmail.com>
2021-03-09 23:03:28 -0500

    spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
 
 import './random-planet.css';
 
@@ -32,30 +33,32 @@ export default class RandomPlanet extends Component {
     
     const { planet:{id, name, population,
       rotationPeriod, diameter } } = this.state;
-     
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
-      </div>
 
-    );
+      return <Spinner />
+     
+    // return (
+    //   <div className="random-planet jumbotron rounded">
+    //     <img className="planet-image"
+    //          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+    //     <div>
+    //       <h4>{name}</h4>
+    //       <ul className="list-group list-group-flush">
+    //         <li className="list-group-item">
+    //           <span className="term">Population</span>
+    //           <span>{population}</span>
+    //         </li>
+    //         <li className="list-group-item">
+    //           <span className="term">Rotation Period</span>
+    //           <span>{rotationPeriod}</span>
+    //         </li>
+    //         <li className="list-group-item">
+    //           <span className="term">Diameter</span>
+    //           <span>{diameter}</span>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+
+    // );
   }
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './spinner.css';
+
+const Spinner =()=> {
+    return (
+        <div class="loadingio-spinner-double-ring-wiu677x2fxl">
+            <div class="ldio-o0k9la10p1s">
+               <div></div>
+               <div></div>
+               <div>
+                   <div></div>
+               </div>
+               <div>
+                   <div></div>
+             </div>
+            </div>
+        </div>
+   
+    );
+};
+export default Spinner;
