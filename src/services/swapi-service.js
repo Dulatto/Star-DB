3f6ad6492a17d7af9cf389ee65557f2c42573db7
Dulatto <dulat.omarov@gmail.com>
2021-03-10 23:13:24 -0500

    lifecycle hooks

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -45,7 +45,7 @@ _extractId(item){
     return item.url.match(idRegExp)[1];
 }
 
-_transformPlanet(planet) {    
+_transformPlanet =(planet) =>{    
     return {
             id: this._extractId(planet),
             name:planet.name,
@@ -54,7 +54,7 @@ _transformPlanet(planet) {
             diameter:planet.diameter  
     }
 }
-_transformStarship(starship) {
+_transformStarship =(starship)=> {
     return {
       id: this._extractId(starship),
       name: starship.name,
@@ -68,7 +68,7 @@ _transformStarship(starship) {
     }
   }
 
-  _transformPerson(person) {
+  _transformPerson =(person)=> {
     return {
       id: this._extractId(person),
       name: person.name,
