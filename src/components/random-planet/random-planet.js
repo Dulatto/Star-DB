c87be4822cfb68f0b72968a705176e8a69b2ca7c
Dulatto <dulat.omarov@gmail.com>
2021-03-10 21:58:32 -0500

    lifecycle hooks

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,6 +17,8 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 5000);
+    clearInterval(this.interval);
   }
 
    onPlanetLoaded = (planet) => {
@@ -33,8 +35,8 @@ export default class RandomPlanet extends Component {
    });
   };
 
-  updatePlanet() {
-    const id = 12;
+  updatePlanet =()=> {
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
