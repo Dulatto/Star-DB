a594dae5362970840256c2c87193263197d14666
Dulatto <dulat.omarov@gmail.com>
2021-03-11 23:53:25 -0500

    lifecycle hooks

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -5,7 +5,7 @@ import './person-details.css';
 
 export default class PersonDetails extends Component {
 
-  swapiService = new SwapiService
+  swapiService = new SwapiService();
 
   state={
     person: null
@@ -17,7 +17,7 @@ export default class PersonDetails extends Component {
 
   updatePerson() {
     const { personId} = this.props;
-    if (personId){
+    if (!personId){
       return;
     }
     this.swapiService
@@ -29,24 +29,31 @@ export default class PersonDetails extends Component {
 
 
   render() {
+     
+    if(!this.state.person){
+      return <span>Select a person from a list</span>
+    }
+
+    const  {id, name, gender, birthYear, eyeColor} = this.state.person;
+
     return (
       <div className="person-details card">
         <img className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Eye Color</span>
+              <span className="term">{eyeColor}</span>
               <span>red</span>
             </li>
           </ul>
