238753c4460cecb7e40f778fa1ae7b5d483da233
Dulatto <dulat.omarov@gmail.com>
2021-03-15 23:56:41 -0400

    ini

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorButton from '../error-button';
-import PeoplePage from '../people-page';
-import ErrorBoundry from "../error-boundry";
+import ErrorBoundry from '../error-boundry';
+
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from '../../services/swapi-service';
+import DummySwapiService from '../../services/dummy-swapi-service';
+
+import { SwapiServiceProvider } from '../swapi-service-context';
+
+import {
+  PersonDetails,
+  PlanetDetails,
+  StarshipDetails,
+  PersonList,
+  PlanetList,
+  StarshipList
+} from '../sw-components';
 
 import './app.css';
 
 export default class App extends Component {
+
+  swapiService = new DummySwapiService();
 
   state = {
     showRandomPlanet: true
@@ -28,24 +43,57 @@ export default class App extends Component {
       <RandomPlanet/> :
       null;
 
+    const { getPerson,
+            getStarship,
+            getPersonImage,
+            getStarshipImage,
+            getAllPeople,
+            getAllPlanets } = this.swapiService;
+
+    const personDetails = (
+      <ItemDetails
+        itemId={11}
+        getData={getPerson}
+        getImageUrl={getPersonImage} >
+
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+
+      </ItemDetails>
+    );
+
+    const starshipDetails = (
+      <ItemDetails
+        itemId={5}
+        getData={getStarship}
+        getImageUrl={getStarshipImage}>
+
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
+      </ItemDetails>
+    );
+
     return (
       <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
-          { planet }
+        <SwapiServiceProvider value={this.swapiService} >
+          <div className="stardb-app">
+            <Header />
 
-          <div className="row mb2 button-row">
-            <button
-              className="toggle-planet btn btn-warning btn-lg"
-              onClick={this.toggleRandomPlanet}>
-              Toggle Random Planet
-            </button>
-            <ErrorButton />
+            <PersonDetails itemId={11} />
+
+            <PlanetDetails itemId={5} />
+
+            <StarshipDetails itemId={9} />
+
+            <PersonList />
+
+            <StarshipList />
+
+            <PlanetList />
+
           </div>
-
-          <PeoplePage />
-
-        </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   }
