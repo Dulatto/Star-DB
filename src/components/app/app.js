7ad34aebdef7ab419b20a464d09798519a2b417a
Dulatto <dulat.omarov@gmail.com>
2021-03-17 23:32:49 -0400

    broken

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,8 +8,10 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 import PeoplePage from '../people-page/people-page';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import ItemDetails from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
+import ErrorBoundry from '../error-boundry/error-boundry';
+import Row from '../row/row';
 
 export default class App extends Component {
 
@@ -43,21 +45,46 @@ export default class App extends Component {
       <RandomPlanet/> :
       null;
 
+      const {getPerson, getStarship, getPersonImage, getStarshipImage} = this.swapiService
+      const personDetails = (
+        <ItemDetails 
+           itemId={11}
+           getData={getPerson}
+           getImageUrl={getPersonImage}/>
+      );
+      const starshipDetails = (
+        <ItemDetails 
+           itemId={5}
+           getData={getStarship}
+           getImageUrl={getStarshipImage}/>
+      );
+
+
     return (
+      <ErrorBoundry>
       <div className="stardb-app">
         <Header />
-        { planet}
+
+       <Row
+       left={personDetails}
+       right={starshipDetails} />
+      </div>
+      </ErrorBoundry>
+    );
+  }
+};
+
+    /* { planet}
         <div className="row ml-2 button-row">
           <button
             className="toggle-planet btn btn-warning btn-lg mr-2"
             onClick={this.toggleRandomPlanet}>
             Toggle Random Planet
-        </button>
-          <ErrorButton />
-        </div>
-        <PeoplePage />
+        </button> 
+         <ErrorButton />
+        </div>*/
 
-        {/* <div className="row mb-2">
+          /* <div className="row mb-2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllPlanets}
@@ -66,9 +93,9 @@ export default class App extends Component {
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
           </div>
-        </div> */}
+        </div> */
 
-        {/* <div className="row mb-2">
+        /* <div className="row mb-2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllStarships}
@@ -77,10 +104,4 @@ export default class App extends Component {
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
           </div>
-        </div> */}
-
-      </div>
-    );
-  }
-};
-
+        </div> */
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import ItemDetails from '../item-details/item-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import Row from '../row/row';
 
@@ -40,7 +40,7 @@ export default class PeoplePage extends Component {
     )
     const personDetails =(
       <ErrorBoundry>
-      <PersonDetails personId={this.state.selectedPerson} />
+      <ItemDetails personId={this.state.selectedPerson} />
       </ErrorBoundry>
     )
 
