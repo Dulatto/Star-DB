3946b0c332c0849ed31abb2922f575384317495b
Dulatto <dulat.omarov@gmail.com>
2021-03-31 23:21:40 -0400

    hoc

--- src/components/sw-components/person-details.js
+++ src/components/sw-components/person-details.js
@@ -5,8 +5,10 @@ import { withSwapiService } from '../hoc-helpers';
 const PersonDetails = (props) => {
     return (
         <ItemDetails {...props} >
+
             <Record field="gender" label="Gender" />
             <Record field="eyeColor" label="Eye Color" />
+
         </ItemDetails>
     );
 };
