7f33028c62916f66505a725bf097753c6d7c2731
Dulatto <dulat.omarov@gmail.com>
2021-04-01 23:31:11 -0400

    hoc

--- src/components/hoc-helpers/with-swapi-service.js
+++ src/components/hoc-helpers/with-swapi-service.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { SwapiServiceConsumer } from '../swapi-service-context';
 
 const withSwapiService = (Wrapped, mapMethodsToProps) => {
-
     return (props) => {
         return (
             <SwapiServiceConsumer>                {
