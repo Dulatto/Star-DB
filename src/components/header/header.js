ebe4a23e182348692923dba5663cae1861a154d4
Dulatto <dulat.omarov@gmail.com>
2021-04-05 21:04:26 -0400

    hoc

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,9 +11,7 @@ const Header = () => {
       <ul className="d-flex">
         <li> <a href="#/people">People</a></li>
         <li> <a href="#/planets">Planets</a></li>
-        <li>
-          <a href="#/starships">Starships</a>
-        </li>
+        <li><a href="#/starships">Starships</a></li>
       </ul>
     </div>
   );
