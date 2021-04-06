490cc457cd7fe0aa91db25ca149d55e0d0d0627a
Dulatto <dulat.omarov@gmail.com>
2021-04-05 21:02:40 -0400

    hoc

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,9 +6,7 @@ const Header = () => {
   return (
     <div className="header d-flex">
       <h3>
-        <a href="#/">
-          StarDB
-        </a>
+        <a href="#/"> StarDB </a>
       </h3>
       <ul className="d-flex">
         <li>
