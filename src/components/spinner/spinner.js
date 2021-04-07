3acd7e71bedb4af15440d3a0fbd57814065d1c4d
Dulatto <dulat.omarov@gmail.com>
2021-04-06 23:27:51 -0400

    initial

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -6,8 +6,8 @@ const Spinner = () => {
   return (
     <div className="lds-css">
       <div className="lds-double-ring">
-        <div></div>
-        <div></div>
+        <div> </div>
+        <div> </div>
       </div>
     </div>
   );
