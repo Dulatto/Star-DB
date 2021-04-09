c9380655ffcc23c6a304fcfad04bb63e8ae242e4
Dulatto <dulat.omarov@gmail.com>
2021-04-08 22:47:58 -0400

    code cleaning

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
-
-import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
-
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import {
@@ -25,7 +21,7 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    swapiService: new DummySwapiService()
+    swapiService: new SwapiService()
   };
 
   onServiceChange = () => {
@@ -33,20 +29,12 @@ export default class App extends Component {
 
       const Service = swapiService instanceof SwapiService ?
         DummySwapiService : SwapiService;
-
       return {
         swapiService: new Service()
       };
     });
   };
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
 
   render() {
 
@@ -60,6 +48,8 @@ export default class App extends Component {
         <SwapiServiceProvider value={this.state.swapiService} >
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
+
+            {planet}
 
             <PersonDetails itemId={11} />
 
