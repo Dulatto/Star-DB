00893612ff32517de543f295eb9f09721159ea78
Dulatto <dulat.omarov@gmail.com>
2021-04-12 23:10:22 -0400

    propTypes

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 import { withData } from '../hoc-helpers';
 import SwapiService from "../../services/swapi-service";
@@ -32,6 +32,13 @@ const ItemList = (props) => {
 ItemList.defaultProps = {
   onItemSelected: () => { }
 }
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
+};
+
 
 const { getAllPeople } = new SwapiService();
 
