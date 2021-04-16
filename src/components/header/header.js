05b727d573fbb2793bf4d56c93aab0b74df0a831
Dulatto <dulat.omarov@gmail.com>
2021-04-15 23:42:53 -0400

    react router

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = ({ onServiceChange }) => {
@@ -11,13 +12,13 @@ const Header = ({ onServiceChange }) => {
       </h3>
       <ul className="d-flex">
         <li>
-          <a href="#/people">People</a>
+          <Link to="/people">People</Link>
         </li>
         <li>
-          <a href="#/planets">Planets</a>
+          <Link to="/planets">Planets</Link>
         </li>
         <li>
-          <a href="#/starships">Starships</a>
+          <Link to="/starships">Starships</Link>
         </li>
       </ul>
 
