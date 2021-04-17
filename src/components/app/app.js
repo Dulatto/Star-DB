92e368b84933ab582f0b172eaffba77f2854c2eb
Dulatto <dulat.omarov@gmail.com>
2021-04-16 23:01:48 -0400

    react router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -46,7 +46,10 @@ export default class App extends Component {
               <Route path='/planets' component={PlanetsPage} />
               <Route path='/starships' exact component={StarshipsPage} />
               <Route path='/starships/:id'
-                render={() => <StarshipDetails />} />
+                render={({ match }) => {
+                  const { id } = match.params;
+                  return <StarshipDetails itemId={id} />
+                }} />
 
             </div>
           </Router>
