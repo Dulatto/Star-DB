1c170b61a4ce1a0e0d8c6fc51b8184c81cd813ac
Dulatto <dulat.omarov@gmail.com>
2021-04-17 23:55:33 -0400

    react router

--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -5,9 +5,7 @@ import { withRouter } from 'react-router-dom';
 const StarshipsPage = ({ history }) => {
     return (
         <StarshipList
-            onItemSelected={(id) => {
-                history.push(id)
-            }} />
+            onItemSelected={(id) => { history.push(id) }} />
     );
 };
 
