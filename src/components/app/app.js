05bf2b3b740e03d20ff5975e24fa940d46508c94
Dulatto <dulat.omarov@gmail.com>
2021-04-18 00:13:05 -0400

    react router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -42,7 +42,7 @@ export default class App extends Component {
               <Route path='/'
                 render={() => <h2>Welcome to StarDB</h2>}
                 exact />
-              <Route path='/people' component={PeoplePage} />
+              <Route path='/people/:id?' component={PeoplePage} />
               <Route path='/planets' component={PlanetsPage} />
               <Route path='/starships' exact component={StarshipsPage} />
               <Route path='/starships/:id'
