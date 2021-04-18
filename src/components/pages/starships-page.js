0fd9c00e0ede2e506adbd78146c96061fa4c153c
Dulatto <dulat.omarov@gmail.com>
2021-04-17 23:18:08 -0400

    react router

--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StarshipList } from '../sw-components';
 import { withRouter } from 'react-router-dom';
 
-
 const StarshipsPage = ({ history }) => {
     return (
         <StarshipList
